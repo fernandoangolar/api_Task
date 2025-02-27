fe78491dd6d147561b85f56f29b4dff493956a96
Fernando Angolar <fernandoangolar581@gmail.com>
2025-02-27 21:29:20 +0100

    :sparkles: feat: Foram integrados os modulos de usuários e tarefas

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { ConfigModule } from "@nestjs/config"
 import { postgresConfigService } from './config/postrgres.config.service';
+import { TaskModule } from './tasks/tasks.module';
+import { UserModule } from './users/users.module';
 
 
 @Module({
     imports: [
+      TaskModule,
+      UserModule,
       ConfigModule.forRoot({
         isGlobal: true
       }),
@@ -16,6 +20,7 @@ import { postgresConfigService } from './config/postrgres.config.service';
         useClass: postgresConfigService,
         inject: [postgresConfigService]
       })
-    ] 
+    ],
+
 })
 export class AppModule {}
